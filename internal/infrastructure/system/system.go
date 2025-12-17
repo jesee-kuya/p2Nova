5227d7f6f84e932864400c082e314f4ea4fbbf48
jkuya <kuyajesee@proton.me>
2025-12-17 23:50:44 +0300

    feat: add AuthService field to P2Nova struct for user authentication

--- internal/infrastructure/system/system.go
+++ internal/infrastructure/system/system.go
@@ -1,7 +1,11 @@
 package system
 
-import authRepo "github.com/jesee-kuya/p2Nova/internal/domain/auth/repository"
+import (
+	authRepo "github.com/jesee-kuya/p2Nova/internal/domain/auth/repository"
+	authService "github.com/jesee-kuya/p2Nova/internal/domain/auth/service"
+)
 
 type P2Nova struct {
 	AuthRepository *authRepo.AuthRepository
+	AuthService    *authService.AuthService
 }
