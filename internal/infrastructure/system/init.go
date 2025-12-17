98e4fb2446a9534ca526c0a4f0715a8a363df2f9
jkuya <kuyajesee@proton.me>
2025-12-18 00:40:43 +0300

    feat: add initialization function for P2Nova with repositories, services, and handlers

---
+++ internal/infrastructure/system/init.go
@@ -0,0 +1,44 @@
+package system
+
+import (
+	"log"
+
+	authRepo "github.com/jesee-kuya/p2Nova/internal/domain/auth/repository"
+	"github.com/jesee-kuya/p2Nova/internal/domain/auth/service"
+	"github.com/jesee-kuya/p2Nova/internal/handler/authHandler"
+	"github.com/jesee-kuya/p2Nova/internal/infrastructure/database"
+)
+
+func Init() *P2Nova {
+	db, err := database.NewConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// repositories
+	authRepository := authRepo.NewAuthRepository(db)
+
+	// services
+	authService := service.NewAuthService(&authRepository)
+
+	// handlers
+	authenticationHandler := authHandler.NewAuthHandler(authService)
+
+	repositories := Repositories{
+		AuthRepository: &authRepository,
+	}
+
+	services := Services{
+		AuthService: &authService,
+	}
+
+	handlers := Handlers{
+		AuthHandler: &authenticationHandler,
+	}
+
+	return &P2Nova{
+		Repositories: repositories,
+		Services:     services,
+		Handlers:     handlers,
+	}
+}
