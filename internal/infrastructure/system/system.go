526c589f6403cef1ea0f26f0f5c8993eca91d369
jkuya <kuyajesee@proton.me>
2025-12-18 00:32:54 +0300

    feat: restructure P2Nova to organize Repositories, Services, and Handlers types

--- internal/infrastructure/system/system.go
+++ internal/infrastructure/system/system.go
@@ -3,9 +3,23 @@ package system
 import (
 	authRepo "github.com/jesee-kuya/p2Nova/internal/domain/auth/repository"
 	authService "github.com/jesee-kuya/p2Nova/internal/domain/auth/service"
+	"github.com/jesee-kuya/p2Nova/internal/handler/authHandler"
 )
 
 type P2Nova struct {
+	Repositories Repositories
+	Services     Services
+	Handlers     Handlers
+}
+
+type Repositories struct {
 	AuthRepository *authRepo.AuthRepository
-	AuthService    *authService.AuthService
+}
+
+type Services struct {
+	AuthService *authService.AuthService
+}
+
+type Handlers struct {
+	AuthHandler *authHandler.AuthHandler
 }
