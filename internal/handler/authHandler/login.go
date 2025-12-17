f432d212751d0320b3310be2ff4f7c6b5bac7dab
jkuya <kuyajesee@proton.me>
2025-12-18 00:29:26 +0300

    feat: implement Login method in AuthHandler for user authentication

---
+++ internal/handler/authHandler/login.go
@@ -0,0 +1,33 @@
+package authHandler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jesee-kuya/p2Nova/internal/handler"
+)
+
+func (h *AuthHandlerImpl) Login(c *gin.Context) {
+	var reqLogin handler.RequestLogin
+
+	if err := c.ShouldBindJSON(&reqLogin); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid request payload",
+		})
+		return
+	}
+
+	user, err := h.AuthService.Login(&reqLogin)
+	if err != nil {
+		c.JSON(http.StatusConflict, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	user.Password = ""
+	c.JSON(http.StatusCreated, gin.H{
+		"user":    user,
+		"message": "login successful, welcome",
+	})
+}
