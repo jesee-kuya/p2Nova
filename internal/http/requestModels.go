177d9233dba96291532af57bc30c6b52d91f9916
jkuya <kuyajesee@proton.me>
2025-12-17 22:47:44 +0300

    feat: add a request user that expects json

---
+++ internal/http/requestModels.go
@@ -0,0 +1,9 @@
+package http
+
+type RequestUser struct {
+	FirstName string `json:"first_name" binding:"required"`
+	LastName  string `json:"last_name" binding:"required"`
+	Email     string `json:"email" binding:"required,email"`
+	Username  string `json:"username" binding:"required"`
+	Password  string `json:"password" binding:"required"`
+}
